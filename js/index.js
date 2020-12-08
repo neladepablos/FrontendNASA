eb87f3e06b014d062144d21d1ded75e52aca38ec
Marianela Ruiz <marianelaruizdepablos@gmail.com>
2020-12-07 19:54:42 -0500

    proyecto NASA

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Incorporar al menos un Array en tu proyecto 
+//  Utilizar algunos de los métodos o propiedades vistos en la clase.
+
+let fotos = ['http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG',
+    'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FRB_486265257EDR_F0481570FHAZ00323M_.JPG',
+    'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/rcam/RLB_486265291EDR_F0481570RHAZ00323M_.JPG',
+    'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/rcam/RRB_486265291EDR_F0481570RHAZ00323M_.JPG'
+]
+
+fotos.forEach(foto => {
+    console.log(foto)
+    console.log(fotos.length);
+
+});
